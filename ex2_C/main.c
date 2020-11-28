8ac74a0701814fafcfcd97b7e673d03625303704
Gabriel Barreto <gabrielbarret4@gmail.com>
2020-11-28 20:23:02 -0300

    Ex2 - Chamadas das funções na classe main

--- ex2_C/main.c
+++ ex2_C/main.c
@@ -1,8 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "produtos.h"
 
-int main()
-{
-    printf("Hello world!\n");
-    return 0;
+int main(){
+    Lista* L1;
+    Lista* L2;
+
+    L1 = criarLista();
+    inserirFinal(L1);
+    system("CLS"); /*Limpando TELA*/
+
+    inserirFinal(L1);
+    system("CLS"); /*Limpando TELA*/
+
+    inserirFinal(L1);
+    system("CLS"); /*Limpando TELA*/
+
+    buscarMenorPreco(L1);
+    system("CLS"); /*Limpando TELA*/
+
+    buscarEstoqueInferior(L1);
+    system("CLS"); /*Limpando TELA*/
+
+    alterarProduto(L1);
+    system("CLS"); /*Limpando TELA*/
+
+    relatorioCrescente(L1);
+    system("CLS"); /*Limpando TELA*/
+
+    relatorioDecrescente(L1);
+
+    L2 = criarLista();
+    inserirFinal(L2);
+    system("CLS"); /*Limpando TELA*/
+
+    inserirFinal(L2);
+    system("CLS"); /*Limpando TELA*/
+
+    inserirFinal(L2);
+    system("CLS"); /*Limpando TELA*/
+
+    buscarMenorPreco(L2);
+    system("CLS"); /*Limpando TELA*/
+
+    buscarEstoqueInferior(L2);
+    system("CLS"); /*Limpando TELA*/
+
+    alterarProduto(L2);
+    system("CLS"); /*Limpando TELA*/
+
+    relatorioCrescente(L2);
+    system("CLS"); /*Limpando TELA*/
+
+    relatorioDecrescente(L2);
 }
