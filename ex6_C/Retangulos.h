8fabaa4678fe0a983fb39823184fe8639f437765
Gabriel Barreto <gabrielbarret4@gmail.com>
2020-11-28 20:42:40 -0300

    Ex6 - Criação e implementação do arquivo Retangulos.h

---
+++ ex6_C/Retangulos.h
@@ -0,0 +1,15 @@
+typedef struct No *Lista;
+
+Lista* criarLista();
+
+int inserirInicio(Lista* inicio);
+
+int inserirFinal(Lista* inicio);
+
+int removerInicio(Lista* inicio);
+
+int removerFinal(Lista* inicio);
+
+int imprimirLista(Lista* inicio);
+
+
