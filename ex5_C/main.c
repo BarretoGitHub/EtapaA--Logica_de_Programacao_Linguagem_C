2d8b5ed7321317629b319a3d177a9d10f6847105
Gabriel Barreto <gabrielbarret4@gmail.com>
2020-11-28 20:38:43 -0300

    Ex5 - Chamando método na classe principal

--- ex5_C/main.c
+++ ex5_C/main.c
@@ -1,8 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "complexos.h"
 
-int main()
-{
-    printf("Hello world!\n");
-    return 0;
+int main(){
+    Complexo* numero1;
+    Complexo* numero2;
+    Complexo* somar;
+    Complexo* subtrair;
+    Complexo* multiplicar;
+    Complexo* dividir;
+
+    /*Criando 2 numeros complexos*/
+    numero1 = criarComplexo(2,6);
+    numero2 = criarComplexo(1,9);
+
+    /*Operações matematicas com COMPLEXOS */
+    somar = somarComplexos(numero1,numero2);
+
+    subtrair = subtrairComplexos(numero1,numero2);
+
+    multiplicar = multiplicarComplexos(numero1,numero2);
+
+    dividir = dividirComplexos(numero1,numero2);
+
+    printf("\n\t\t<< Numeros COMPLEXOS criados >>\n");
+    printf("\nNumero 1: ");
+    imprimirComplexo(numero1);
+
+    printf("\nNumero 2: ");
+    imprimirComplexo(numero2);
+
+    printf("\n_________________________________________________________\n");
+
+    printf("\n\n\t<< Efetuando operacoes Matematicas com os COMPLEXOS criados >>\n");
+
+    printf("\nSoma: ");
+    imprimirComplexo(somar);
+
+    printf("\nSubtracao: ");
+    imprimirComplexo(subtrair);
+
+    printf("\nMultiplicacao: ");
+    imprimirComplexo(multiplicar);
+
+    printf("\nDivisao: ");
+    imprimirComplexo(dividir);
+
+
+    printf("\n_________________________________________________________\n");
+
+    system("PAUSE");
 }
