482d002d282c33acaa40bae8a807c4d139c47914
Gabriel Barreto <gabrielbarret4@gmail.com>
2020-11-28 20:43:30 -0300

    Ex6 - Chamando métodos na classe principal

--- ex6_C/main.c
+++ ex6_C/main.c
@@ -1,8 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "Retangulos.h"
 
-int main()
-{
-    printf("Hello world!\n");
-    return 0;
+int main(){
+    Lista lista;
+
+    lista = criarLista();
+
+    inserirInicio(lista);
+    printf("\n_______________________________________\n");
+
+    inserirFinal(lista);
+    printf("\n_______________________________________\n");
+
+    inserirInicio(lista);
+    printf("\n_______________________________________\n");
+
+    printf("\n\t<< Lista de Retangulos >>\n\n");
+    imprimirLista(lista);
+
+    getch();
+
+    printf("\n\n.....REMOVENDO FINAL.....\n");
+    removerFinal(lista);
+
+    /*Caso desejar imprimir*/
+    /*imprimirLista(lista); */
+
+    getch();
+
+    printf("\n\n.....REMOVENDO INICIO .....\n");
+    removerInicio(lista);
+
+   /*Caso desejar imprimir*/
+   /*imprimirLista(lista); */
+
 }
