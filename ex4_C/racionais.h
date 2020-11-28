d2fe3fffbedc1cc8a71e536fd147942e514035e0
Gabriel Barreto <gabrielbarret4@gmail.com>
2020-11-28 20:32:16 -0300

    Ex4 - Criação e implemetação do arquivo racionais.h

---
+++ ex4_C/racionais.h
@@ -0,0 +1,10 @@
+typedef struct racional Numero;
+
+Numero* criarRacional(int num,int den);
+
+Numero* somarRacional(Numero* numero1,Numero* numero2);
+
+Numero* multiplicarRacional(Numero* numero1, Numero* numero2);
+
+int testarRacionais(Numero* numero1,Numero* numero2);
+
