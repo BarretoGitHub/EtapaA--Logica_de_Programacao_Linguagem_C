a1bc164b014cdeb41519b2c142e19ddefa202465
Gabriel Barreto <gabrielbarret4@gmail.com>
2020-11-28 20:37:09 -0300

    Ex5 - Criação e implementação do arquivo complexos.h

---
+++ ex5_C/complexos.h
@@ -0,0 +1,15 @@
+typedef struct complexo Complexo;
+
+Complexo* criarComplexo(int a, int b);
+
+Complexo* somarComplexos(Complexo* num1, Complexo* num2);
+
+Complexo* subtrairComplexos(Complexo* num1, Complexo* num2);
+
+Complexo* multiplicarComplexos(Complexo* num1, Complexo* num2);
+
+Complexo* dividirComplexos(Complexo* num1, Complexo* num2);
+
+void imprimirComplexo(Complexo* num);
+
+
