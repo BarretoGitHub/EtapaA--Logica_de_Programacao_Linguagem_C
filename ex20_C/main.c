9f77e45f06dd93d80d78527c3783ef74a6d7ed56
Gabriel Barreto <gabrielbarret4@gmail.com>
2020-11-28 21:42:11 -0300

    Ex20 - Desenvolvimento da lógica de recursividade

--- ex20_C/main.c
+++ ex20_C/main.c
@@ -1,8 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
+int escolherTermo();
+int funcaoRecursiva(int);
+
+int main(){
+    int termo;
+    int i;
+
+    termo = escolherTermo();
+
+
+    for(i = 0; i < termo; i++){
+        printf("\nTermo (%d) = (%d)\n",(i + 1),funcaoRecursiva(i));
+    }
+
+
+    printf("\n\n");
+    system("PAUSE");
+
     return 0;
+}
+
+int escolherTermo(){
+    int termo;
+
+    printf("Insira o termo de FIBONACCI: ");
+    scanf("%d",&termo);
+
+
+    return(termo);
+}
+
+int funcaoRecursiva(int termo){
+    int flag;
+
+    if(termo == 0){
+        flag = 0;
+    }
+
+    else if(termo == 1 || termo == 2){
+        flag = 1;
+    }
+
+    else{
+        flag = funcaoRecursiva(termo - 1) + funcaoRecursiva(termo - 2);
+    }
+
+    return (flag);
 }
