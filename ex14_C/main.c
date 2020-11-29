eaa77a984c7bdc4601191d42c4ec2b48eca3de6a
Gabriel Barreto <gabrielbarret4@gmail.com>
2020-11-28 21:22:32 -0300

    Ex14 - Chamada de métodos na main()

--- ex14_C/main.c
+++ ex14_C/main.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "Lista.h"
 
-int main()
-{
-    printf("Hello world!\n");
-    return 0;
+int main(){
+    Lista* lista;
+
+    lista = criarLista();
+
+    inserirLista(lista);
+    inserirLista(lista);
+    inserirLista(lista);
+    inserirLista(lista);
+    inserirLista(lista);
+
+    printf("\n______________________________\n");
+    printf("\n\t\t << LISTA DE NUMEROS >>\n\n");
+    imprimirLista(lista);
+
+
+    removerLista(lista);
+    printf("\n\nRemovendo NUMERO da Lista..........\n\n");
+    getch();
+
+    printf("\n\n\t\t<< Lista Atualizada >>>\n\n");
+    imprimirLista(lista);
 }
