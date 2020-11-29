08b3d3f442c4a59263cc2be7754cf44b3fa273df
Gabriel Barreto <gabrielbarret4@gmail.com>
2020-11-28 22:34:58 -0300

    Ex30 - Chamada de metodos na main()

--- ex30_C/main.c
+++ ex30_C/main.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "Arvore.h"
 
-int main()
-{
-    printf("Hello world!\n");
+int main(){
+    ArvBin* a = cria_ArvBin();
+    ArvBin* b = cria_ArvBin();
+
+    insere_ArvBin(a, 12);
+    insere_ArvBin(a, 12);
+
+    insere_ArvBin(b, 10);
+    insere_ArvBin(b, 12);
+
+    //CHAMADA DA FUNÇÃO QUE VERIFICA SE AS DUAS ÁRVORES BINÁRIAS SÃO IGUAIS
+    igual(a, b);
+
+    libera_ArvBin(a);
+    libera_ArvBin(b);
+
+    printf("\n\n\n");
+
     return 0;
 }
