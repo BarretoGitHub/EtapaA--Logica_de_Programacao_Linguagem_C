036ec6a039527b8fc8e7eafd4c7ff0835e2fa54b
Gabriel Barreto <gabrielbarret4@gmail.com>
2020-11-28 21:03:21 -0300

    Ex10 - Criação e implementação do arquivo Pilha.h

---
+++ ex10_C/Pilha.h
@@ -0,0 +1,15 @@
+typedef struct pilha Pilha;
+
+Pilha* criaPilha();
+
+int pushPilha1(Pilha* pilha,int num);
+
+int popPilha1(Pilha* pilha);
+
+int pushPilha2(Pilha* pilha,int num);
+
+int popPilha2(Pilha* pilha);
+
+int imprimePilha1(Pilha* pilha);
+
+
