18fb274ca38ee54574dc3ef38c5d92d23bfa22f2
Gabriel Barreto <gabrielbarret4@gmail.com>
2020-11-28 22:56:27 -0300

    Efetuando alterações

--- ex3_C/main.c
+++ ex3_C/main.c
@@ -1,8 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "Lista.h"
 
-int main()
-{
-    printf("Hello world!\n");
-    return 0;
+int main(){
+
+    Lista* L1;
+    Lista* L2;
+    ListaM* merge;
+
+    L1 = criarLista();
+
+    inserirInicio(L1,12);
+
+    inserirInicio(L1,23);
+
+    inserirFinal(L1,34);
+
+    inserirFinal(L1,45);
+
+    inserirInicio(L1,56);
+
+    inserirInicio(L1,67);
+
+    printf("\n______________________________________\n");
+    imprimirLista(L1);
+
+
+    L2 = criarLista();
+
+    inserirInicio(L2,78);
+
+    inserirInicio(L2,89);
+
+    inserirFinal(L2,90);
+
+    inserirFinal(L2,98);
+
+    inserirInicio(L2,87);
+
+    inserirInicio(L2,76);
+
+
+    printf("\n______________________________________\n");
+
+    imprimirLista(L2);
+
+    printf("\n______________________________________\n");
+
+    merge = mergeListas(L1,L2);
+
+
+    printf("\n\n\t<< MERGE DAS LISTAS >> \n\n");
+    imprimirListaMerge(merge);
+
+    printf("\n______________________________________\n");
 }
--- ex7_C/main.c
+++ ex7_C/main.c
@@ -1,8 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "ListaHeterogenea.h"
+#define RET 0
+#define TRI 1
+#define CIR 2
 
-int main()
-{
-    printf("Hello world!\n");
-    return 0;
+int main(){
+    Lista *L1;
+
+    L1 = criarLista();
+
+    inserirFinal(L1,RET);
+
+    inserirInicio(L1,TRI);
+
+    inserirInicio(L1,CIR);
+
+    inserirFinal(L1,TRI);
+
+    inserirFinal(L1,CIR);
+
+    getch();
+    printf("\n\n_______________________________________________\n\n");
+
+
+    printf("\n<<LISTA >>\n\n");
+    imprimirLista(L1);
+
+    getch();
+    printf("Removendo Final .............\n");
+    removerFinal(L1);
+    printf("\n\n_______________________________________________\n\n");
+
+    getch();
+    printf("Removendo Inicio .............\n");
+    removerInicio(L1);
+
+    printf("\n\n_______________________________________________\n\n");
+
+    printf("\n<< LISTA ATUALIZADA >>\n\n");
+    imprimirLista(L1);
+
 }
