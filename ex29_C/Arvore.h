ea8093df51b968ed585b057c79275b693b798bd6
Gabriel Barreto <gabrielbarret4@gmail.com>
2020-11-28 22:25:51 -0300

    Ex29 - Criação e desnvolvimento do arquivo Arvore.h

---
+++ ex29_C/Arvore.h
@@ -0,0 +1,12 @@
+typedef struct arvore Arvore;
+
+Arvore* criarArvore(int altura);
+
+int imprimeArvore(Arvore* arvore,int elementos);
+
+int inserirElemento1(Arvore* arvore);
+
+int inserirElemento(Arvore* arvore, int indice);
+
+
+
